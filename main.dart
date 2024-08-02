f56921834ca84cb0c8b97fe518a9a75f688528d9
Ali Karimi <a.karimi2729@gmail.com>
2024-08-02 20:51:51 +0330

    call methods to aliens

--- main.dart
+++ main.dart
@@ -1,3 +1,15 @@
+import 'alien.dart';
+import 'martian.dart';
+import 'saturn.dart';
+
 void main(List<String> args) {
-  
+  // defining some Aliens
+  Alien Zephyrites = Martian(2, 24, 2, 'white', 1);
+  Alien Aeronids = Saturn(5, 12, 4, 'black', 1);
+  Alien Xylaphs = Alien(10, 0, 4, 'pink');
+
+  // aliens move !
+  Zephyrites.move();
+  Aeronids.move();
+  Xylaphs.move();
 }
